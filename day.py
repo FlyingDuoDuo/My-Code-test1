265d06daa6388cc1cd1e4d92b2b5597601793284
wang <gg1532565847@gmail.com>
2020-02-19 23:21:44 +0800

    day

---
+++ day.py
@@ -0,0 +1,4 @@
+days=int(input('Enter days:'))
+month=days//30
+days=days%30
+print('Month:{} Days:{}'.format(month,days))
---
+++ day2.py
@@ -0,0 +1,2 @@
+days=int(input('Enter days:'))
+print('Month:{},days:{}'.format(*divmod(days,30)))
